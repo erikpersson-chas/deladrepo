f0a7584487119488a4e1e7f42e2885bf51e14e2b
Erik Persson <erik.persson@chasacademy.se>
2025-09-10 17:02:48 +0200

    html body

--- ep_prezent/js/js.js
+++ ep_prezent/js/js.js
@@ -12,17 +12,47 @@ const add_classes = (el, class_string) => {
     el.classList.add(...class_list);
 };
 
-const header = create_el('header');
+const header = create_el("header");
 header.id = "header";
 add_classes(header, "");
 
-const main = create_el('main');
+const header_h1 = create_el("h1");
+header_h1.innerHTML = "S A D B O Y S";
+add_classes(header_h1, "");
+
+header.appendChild(header_h1);
+
+const header_p1 = create_el("p");
+header_p1.innerHTML = "Det här är sagan om";
+add_classes(header_p1, "");
+
+header.appendChild(header_p1);
+
+const header_p2 = create_el("p");
+header_p2.innerHTML = "coolaste katten i det Långa Landet Falukorv";
+add_classes(header_p2, "");
+
+header.appendChild(header_p2);
+
+const main = create_el("main");
 main.id = "main";
 add_classes(main, "");
 
-const footer = create_el('footer');
+const main_p1 = create_el("p");
+main_p1.innerHTML = "echo -e \"inga gäris bara habbar\" >> sysloggen # de e för tjejorna från södra latin som vi göre för";
+add_classes(main_p1, "");
+
+main.appendChild(main_p1);
+
+const footer = create_el("footer");
 footer.id = "footer";
 add_classes(footer, "");
+
+const footer_p1 = create_el("p");
+footer_p1.innerHTML = "&copy Erik Persson";
+add_classes(footer_p1, "");
+
+footer.appendChild(footer_p1);
 
 const body = qs('#body');
 body.appendChild(header);
